bf8dde3197665485bb6dc0ceea15d9e508917aae
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2022-11-06 18:17:31 +0200

    refactor: moved fetching out of container

--- react-movierama/src/components/CardContainer.tsx
+++ react-movierama/src/components/CardContainer.tsx
@@ -1,6 +1,5 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import styled from "styled-components";
-import useMovieDB from "../hooks/useMovieDB";
 import Card from "./Card";
 
 const Grid = styled.div`
@@ -18,55 +17,67 @@ const Grid = styled.div`
 
 interface ICardContainerProps {
   cards: any[];
+  infinite: boolean;
+  onIntersect?: Function;
+  hasMore?: boolean;
 }
 
-function CardContainer({ cards }: ICardContainerProps) {
-  const [movies, setMovies] = useState<any[]>([]);
+type OptionalCardContainerProps =
+  | ({ infinite: false } & ICardContainerProps)
+  | ({ infinite: true } & Required<ICardContainerProps>);
 
-  const { useFetchNowPlaying } = useMovieDB(
-    process.env.REACT_APP_MOVIEDB_API_KEY || ""
-  );
-
-  const { isLoading, data, hasMore, page, fetchNext } = useFetchNowPlaying();
+function CardContainer({
+  cards,
+  infinite = false,
+  onIntersect,
+  hasMore,
+}: OptionalCardContainerProps) {
   const observer: any = useRef();
-
-  useEffect(() => {
-    if (isLoading) return;
-    setMovies((prev) => [...prev, ...(data || [])]);
-  }, [isLoading, data, setMovies]);
 
   const lastCardListItem = useCallback(
     (node: any) => {
-      if (isLoading) return;
       if (observer.current) observer.current.disconnect();
+
+      if (!infinite) return;
       observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          fetchNext();
+        if (entries[0].isIntersecting && hasMore && onIntersect) {
+          onIntersect();
         }
       });
       if (node) observer.current.observe(node);
     },
-    [isLoading, hasMore, fetchNext]
+    [infinite, onIntersect, hasMore]
   );
 
-  const getMovieTitles = () => {
-    return movies.map((movie: any, index: number) => (
-      <Card
-        ref={movies.length === index + 1 ? lastCardListItem : undefined}
-        key={`${movie.id}-${page}`}
-        id={movie.id}
-        poster={movie.poster_path}
-        title={movie.title || movie.original_title}
-        releaseYear={movie.release_date}
-        genres={movie.genre_ids}
-        rating={movie.vote_average}
-        ratingCount={movie.vote_count}
-        overview={movie.overview}
-      />
-    ));
+  const mapStateToCards = () => {
+    const ids = new Set();
+    return cards.map((movie: any, index: number) => {
+      /**
+       * Issue with Movie DB sometimes responsing on different pages
+       * with the same movie, thus causing issues on Virtual DOM as
+       * some elements ends up having the same key.
+       */
+      if (ids.has(movie.id)) return undefined;
+      ids.add(movie.id);
+
+      return (
+        <Card
+          ref={cards.length === index + 1 ? lastCardListItem : undefined}
+          key={movie.id}
+          id={movie.id}
+          poster={movie.poster_path}
+          title={movie.title || movie.original_title}
+          releaseYear={movie.release_date}
+          genres={movie.genre_ids}
+          rating={movie.vote_average}
+          ratingCount={movie.vote_count}
+          overview={movie.overview}
+        />
+      );
+    });
   };
 
-  return <Grid>{getMovieTitles()}</Grid>;
+  return <Grid>{mapStateToCards()}</Grid>;
 }
 
 export default CardContainer;
